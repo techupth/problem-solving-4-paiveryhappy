c7cc81e5e9837d716ae44d7d05978b1ac51122ab
Jiran <paiveryhappy@gmail.com>
2024-04-29 21:35:51 +0700

    faet: add problem-solving function

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,19 @@
-function pivotIndex(nums) {}
+function pivotIndex(nums) {
+  let totalSum = 0;
+  let leftSum = 0;
+
+  nums.forEach((element) => {
+    totalSum += element;
+  });
+
+  for (let i = 0; i < nums.length; i++) {
+    if (totalSum - leftSum - nums[i] === leftSum) {
+      console.log(i);
+    }
+    leftSum += nums[i];
+  }
+  console.log(-1);
+}
+
+pivotIndex([1, 7, 3, 6, 5, 6]);
+pivotIndex([1, 2, 3]);
--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,14 @@
-function defangIPaddr(nums) {}
+function defangIPaddr(ipAddress) {
+  let newIpAddress = "";
+  for (let i = 0; i < ipAddress.length; i++) {
+    if (ipAddress[i] === ".") {
+      newIpAddress += "[.]";
+    } else {
+      newIpAddress += ipAddress[i];
+    }
+  }
+  console.log(newIpAddress);
+}
+
+defangIPaddr("1.1.1.1");
+defangIPaddr("255.100.50.0");
